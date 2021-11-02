990f488a8b49b8f443a08ae9238915755a74a416
Todd J. Lee <tjltm83@gmail.com>
2021-11-01 19:19:42 -0700

    HW verification sketch.

---
+++ SW/HWValidation/HWValidation.ino
@@ -0,0 +1,60 @@
+#define Output1 3
+#define Output2 4
+#define Output3 5
+#define Quarter 6
+#define Half 7
+#define ThreeQuarter 8
+#define Full 9
+String LastLevel;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Output1,OUTPUT);
+  pinMode(Output2,OUTPUT);
+  pinMode(Output3,OUTPUT);
+  pinMode(Quarter,INPUT);
+  pinMode(Half,INPUT);
+  pinMode(ThreeQuarter,INPUT);
+  pinMode(Full,INPUT);
+  Serial.println("Starting up");
+}
+
+void loop() {
+digitalWrite(Output1,HIGH); // Energize the Galvanic sensors
+ReadTankLevel();
+digitalWrite(Output1,LOW); // Turn off the sensors 
+delay(1000);
+}
+
+void ReadTankLevel(){
+  delay(250); // delay to allow the sensors to be powered up. 
+  byte TankStatus;
+  bitWrite(TankStatus,0,digitalRead(Quarter));
+  bitWrite(TankStatus,1,digitalRead(Half));
+  bitWrite(TankStatus,2,digitalRead(ThreeQuarter));
+  bitWrite(TankStatus,3,digitalRead(Full));
+
+  switch(TankStatus){
+     case 0x0000:
+      LastLevel = "Empty";
+      break;
+    case 0x0001:
+      LastLevel = "1/4";
+      break;
+    case 0x0011:
+      LastLevel = "1/2";
+      break;
+    case 0x0111:
+      LastLevel = "3/4";
+      break;
+    case 0x1111:
+      LastLevel = "Full";
+      break;
+    default:
+      LastLevel = "ERROR Check Tank";
+    break;
+  }
+
+Serial.print("Tank Level: ");
+Serial.println(LastLevel);
+}
